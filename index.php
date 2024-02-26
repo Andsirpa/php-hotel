ed76eb74c114119df11ca17549705aa248fe4c04
Andsirpa <andsirpa94@gmail.com>
2024-02-26 17:50:37 +0100

    add stampa dati a schermo

--- index.php
+++ index.php
@@ -8,6 +8,26 @@
     <title>Php Hotels</title>
 </head>
 <body>
-    
+    <h1>HOTELS</h1>
+<!-- stampo in una tabella -->
+    <table>
+  <tr>
+    <th>Nome</th>
+    <th>Descrizione</th>
+    <th>Parcheggio</th>
+    <th>Voto</th>
+    <th>Distanza dal centro</th>
+  </tr>
+  <?php foreach ($hotels as $hotel): ?>
+    <tr>
+      <td><?=  $hotel["name"]; ?></td>
+      <td><?=  $hotel["description"]; ?></td>
+      <td><?=  $hotel["parking"] ? "Si" : "No"; ?></td>
+      <td><?=  $hotel["vote"]; ?></td>
+      <td><?=  $hotel["distance_to_center"]; ?></td>
+    </tr>
+  <?php endforeach; ?>
+</table>
+
 </body>
 </html>
