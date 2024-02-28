e58f6e9d7e5390340c68036d8ff2d334d7b4f07e
Andsirpa <andsirpa94@gmail.com>
2024-02-28 22:13:21 +0100

    feat: separati dati, logica e template e fixato filtri

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
-<?php require_once "./init.php"; ?>
+<?php require_once __DIR__ . "./init.php"; ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,6 +12,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
     <div class="container">
         <div class="text-center mt-5">
@@ -19,18 +21,20 @@
 
         <!-- aggiungo il form per i filtri -->
         <form action="index.php" method="get" class="d-flex justify-content-center mb-3">
+            <!-- seleziona parcheggio -->
             <label for="parking">Parcheggio:</label>
             <input type="checkbox" name="parking" id="parking" class="form-check-input me-3">
             <br>
-            <!-- filtro per voto -->
-           <label for="vote" class="me-2">Voto minimo:</label>
-                <select name="vote" id="vote" class="form-select w-25">
+            <!-- seleziona il voto -->
+            <label for="vote" class="me-2">Voto minimo:</label>
+            <select name="vote" id="vote" class="form-select w-25">
                 <option value="">Tutti</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
+                <!-- faccio in modo che il voto selezionato resti visibile -->
+                <option value="1" <?= $vote == 1 ? "selected" : "" ?>>1</option>
+                <option value="2" <?= $vote == 2 ? "selected" : "" ?>>2</option>
+                <option value="3" <?= $vote == 3 ? "selected" : "" ?>>3</option>
+                <option value="4" <?= $vote == 4 ? "selected" : "" ?>>4</option>
+                <option value="5" <?= $vote == 5 ? "selected" : "" ?>>5</option>
             </select>
 
             <br>
@@ -49,15 +53,26 @@
             </tr>
             <?php foreach ($hotels as $hotel): ?>
                 <tr>
-                <td><?=  $hotel["name"]; ?></td>
-                <td><?=  $hotel["description"]; ?></td>
-                <td><?=  $hotel["parking"] ? "Si" : "No"; ?></td>
-                <td><?=  $hotel["vote"]; ?></td>
-                <td><?=  $hotel["distance_to_center"]; ?></td>
+                    <td>
+                        <?= $hotel["name"]; ?>
+                    </td>
+                    <td>
+                        <?= $hotel["description"]; ?>
+                    </td>
+                    <td>
+                        <?= $hotel["parking"] ? "Si" : "No"; ?>
+                    </td>
+                    <td>
+                        <?= $hotel["vote"]; ?>
+                    </td>
+                    <td>
+                        <?= $hotel["distance_to_center"]; ?>
+                    </td>
                 </tr>
             <?php endforeach; ?>
         </table>
     </div>
 
 </body>
+
 </html>
--- init.php
+++ init.php
@@ -1,62 +1,20 @@
+<?php require_once __DIR__ . "./hotels.php";
 
-<?php
+// uso il Null Coaleshing Operator per una sintassi più leggibile
+// Filtro per parcheggio
+$parking = $_GET['parking'] ?? null;
+if ($parking === 'on') {
+    $hotels = array_filter($hotels, function ($hotel) {
+        return $hotel['parking'];
+    });
+}
 
-    $hotels = [
-
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
-
-    ];
-
-    // uso il Null Coaleshing Operator per una sintassi più leggibile
-    // Filtro per parcheggio
-    $parking = $_GET['parking'] ?? null;
-    if ($parking === 'on') {
-        $hotels = array_filter($hotels, function($hotel) {
-            return $hotel['parking'];
-        });
-    }
-
-    // Filtro per voto
-    $vote = (int) ($_GET['vote'] ?? 0);
-        if ($vote > 0) {
-        $hotels = array_filter($hotels, function($hotel) use ($vote) {
-            return $hotel['vote'] >= $vote;
-        });
-    }
-
+// Filtro per voto
+$vote = (int) ($_GET['vote'] ?? 0);
+if ($vote > 0) {
+    $hotels = array_filter($hotels, function ($hotel) use ($vote) {
+        return $hotel['vote'] >= $vote;
+    });
+}
 
 ?>
